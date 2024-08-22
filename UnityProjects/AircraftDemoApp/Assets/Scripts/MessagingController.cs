3c29679e8e4ec87bc33b29e2c16aea6b1db71bb9
Rlee08 <rlee08@risd.edu>
2024-08-22 14:07:22 +0530

    implement Instantiate Speech Bubble on ButtonPressed

--- UnityProjects/AircraftDemoApp/Assets/Scripts/MessagingController.cs
+++ UnityProjects/AircraftDemoApp/Assets/Scripts/MessagingController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+// using System.Numerics;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
@@ -8,8 +9,33 @@ using UnityEngine;
 public class MessagingController : MonoBehaviour
 {
     public static string dictationResult;
+    public GameObject messageRightPrefab;
+    [SerializeField] GameObject messagesContainer;
+    public int userMessageCount;
+    private GameObject messageRightClone;
 
-    public void DictationResult(string text)
+    // Makes a new speech bubble for every new recording
+    public void MakeNewMessage()
+    {
+        userMessageCount ++ ;
+        messageRightClone = Instantiate(messageRightPrefab);
+        messageRightClone.transform.SetParent(messagesContainer.transform, false);
+        // messageRightClone.transform.position = new Vector3(0,0,0);
+        // messageRightClone.transform.localScale = new Vector3(1,1,1);
+        messageRightClone.name = "MessageRight" + userMessageCount;
+        StartCoroutine(UpdateLayoutGroup(messageRightClone));
+    }
+
+    // Forces the layout group to update to fix the formatting
+    IEnumerator UpdateLayoutGroup(GameObject prefabInstance)
+    {
+        yield return new WaitForEndOfFrame();
+        prefabInstance.SetActive(false);
+        prefabInstance.SetActive(true);
+    }
+
+    // Gets the dictation result into a string
+    public void OutputDictationResult(string text)
     {
         dictationResult = text;
         Debug.Log(dictationResult);
@@ -18,7 +44,7 @@ public class MessagingController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        userMessageCount = 0;
     }
 
     // Update is called once per frame
